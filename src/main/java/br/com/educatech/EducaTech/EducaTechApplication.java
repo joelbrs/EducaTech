e8d4fcd82f45e02d00a234522a506da60ba140e3
Joel Filho <joelfilho02812@gmail.com>
2023-11-12 13:37:51 -0300

    project created & config: security & application.properties

---
+++ src/main/java/br/com/educatech/EducaTech/EducaTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.educatech.EducaTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/educatech/EducaTech/EducaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.educatech.EducaTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
